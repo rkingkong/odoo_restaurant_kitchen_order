6255cfecbcc9788119ae9aa2627e168939c09e7f
Rodolfo Kong <kongrodolfo@gmail.com>
2025-09-13 21:19:08 -0600

    Update pos_config.py

--- models/pos_config.py
+++ models/pos_config.py
@@ -1,27 +1,17 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
     
     auto_print_kitchen = fields.Boolean(
-        string='Comandas de Cocina Automáticas',
+        string='Auto Print Kitchen Orders',
         default=True,
-        help='Imprimir automáticamente comandas de cocina al validar pagos'
-    )
-    
-    print_kitchen_on_payment = fields.Boolean(
-        string='Imprimir al Pagar',
-        default=True
+        help='Automatically print kitchen orders when validating payment'
     )
     
     enable_kitchen_reprint = fields.Boolean(
-        string='Botón de Reimpresión',
+        string='Enable Kitchen Reprint Button',
         default=True,
-        help='Mostrar botón para reimprimir comandas en el POS'
-    )
-    
-    kitchen_categories = fields.Many2many(
-        'pos.category',
-        string='Categorías de Cocina',
-        help='Categorías de productos que se envían a cocina'
+        help='Show button to reprint kitchen orders in POS'
     )
